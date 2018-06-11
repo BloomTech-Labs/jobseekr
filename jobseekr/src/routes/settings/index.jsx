e904bb09e8d6b275c1e0df913d5561887671443b
Aaron Burk <aarondburk@gmail.com>
2018-06-11 17:43:30 -0500

    add basic form control box using bootstrap boilerplate

--- jobseekr/src/routes/settings/index.jsx
+++ jobseekr/src/routes/settings/index.jsx
@@ -1,17 +1,54 @@
 import React from 'react';
+import { FormGroup, FormControl, ControlLabel, HelpBlock } from 'react-bootstrap';
 import Header from '../../components/header';
 
-const Settings = () =>
-  (
-    <div className="parent">
-      <Header />
-      <div className="SettingsWrapper">
-        <div>
-          <h4>hello im Settings
-          </h4>
-        </div>
+
+class Settings extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      value: '',
+    };
+  }
+
+  getValidationState() {
+    const { length } = this.state.value;
+    if (length > 10) return 'success';
+    else if (length > 5) return 'warning';
+    else if (length > 0) return 'error';
+    return null;
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  render() {
+    return (
+      <div className="parent">
+        <Header />
+        <form>
+          <FormGroup
+            controlId="formBasicText"
+            validationState={this.getValidationState()}
+          >
+            <ControlLabel>Working example with validation</ControlLabel>
+            <FormControl
+              type="text"
+              value={this.state.value}
+              placeholder="Enter text"
+              onChange={this.handleChange}
+            />
+            <FormControl.Feedback />
+            <HelpBlock>Validation is based on string length.</HelpBlock>
+          </FormGroup>
+        </form>
       </div>
-    </div>
-  );
+    );
+  }
+}
 
 export default Settings;
