7c9c5985192a69d50e5cc98ab0f48f099febef48
Jonathan Bry <jonathanbry@comcast.net>
2018-06-11 14:39:18 -0700

    added more routes and finalized structure for the most part

--- jobseekr/src/App.jsx
+++ jobseekr/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import AddJob from "./components/addJob";
-import Billing from "./components/billing";
-import Contributions from "./components/contributions";
-import Jobs from "./components/jobs";
-import LandingPage from "./components/landingPage";
-import MeetUps from "./components/meetUps";
-import Settings from "./components/settings";
-import SignIn from "./components/signIn";
-import SignOut from "./components/signOut";
+import LandingPage from "./routes/landingPage";
+import AddJob from "./routes/addJob";
+import Billing from "./routes/billing";
+import Contributions from "./routes/contributions";
+import Jobs from "./routes/jobs";
+import MeetUps from "./routes/meetUps";
+import Settings from "./routes/settings";
+import SignIn from "./routes/signIn";
+import SignOut from "./routes/signOut";
 import Header from "./components/header";
 
 const JobSeeker = () => (
@@ -16,6 +16,12 @@ const JobSeeker = () => (
     <div>
       <Header />
       <Route exact path="/" component={LandingPage} />
+      <Route path="/addJob" component={AddJob} />
+      <Route path="/billing" component={Billing} />
+      <Route path="/contributions" component={Contributions} />
+      <Route path="/jobs" component={Jobs} />
+      <Route path="/meetUps" component={MeetUps} />
+      <Route path="/settings" component={Settings} />
       <Route path="/signIn" component={SignIn} />
       <Route path="/signOut" component={SignOut} />
     </div>
---
+++ jobseekr/src/routes/landingPage/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navbar, NavItem, NavDropdown, Nav, MenuItem } from 'react-bootstrap';
+
+const LandingPage = () =>
+  (
+    <div className="parent">
+      <Header />
+      <div className="LandingWrapper">
+        <div>
+        </div>
+      </div>
+    </div>
+  );
+
+export default LandingPage;
