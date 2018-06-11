78b245693705fff7f402f7c6ad8dcee03f270ba7
Jonathan Bry <jonathanbry@comcast.net>
2018-06-11 13:52:25 -0700

    added router to app.js

--- jobseekr/src/App.js
+++ jobseekr/src/App.js
@@ -1,24 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Jumbotron, Button } from 'react-bootstrap';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Header from "./components/header";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-				<Jumbotron>
-  <h1>JobSeekr!</h1>
-  <p>
-		Find your next job quickly and in an organized fashion!
-  </p>
-  <p>
-    <Button bsStyle="primary">Buy Now!</Button>
-  </p>
-</Jumbotron>
-      </div>
-    );
-  }
-}
+const JobSeeker = () => (
+  <Router>
+    <div>
+      <Header />
+      <Route exact path="/" component={Home} />
+      <Route path="/signIn" component={About} />
+      <Route path="/signOut" component={Topics} />
+    </div>
+  </Router>
+);
 
-export default App;
+
+export default JobSeeker;
+
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import { Jumbotron, Button } from 'react-bootstrap';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+// 				<Jumbotron>
+//   <h1>JobSeekr!</h1>
+//   <p>
+// 		Find your next job quickly and in an organized fashion!
+//   </p>
+//   <p>
+//     <Button bsStyle="primary">Buy Now!</Button>
+//   </p>
+// </Jumbotron>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
