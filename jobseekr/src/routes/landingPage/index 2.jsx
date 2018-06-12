3be898691cf689326c8e519deb40971780c76459
Aaron Burk <aarondburk@gmail.com>
2018-06-12 13:20:01 -0500

    fixed linter error where the linter said document was not defined in index.js

---
+++ jobseekr/src/routes/landingPage/index 2.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../../components/header';
+
+const LandingPage = () =>
+  (
+    <div className="parent">
+      <Header />
+      <div className="LandingWrapper">
+        <div className="first">
+          <h4>hello im landing page
+          </h4>
+        </div>
+      </div>
+    </div>
+  );
+
+export default LandingPage;
