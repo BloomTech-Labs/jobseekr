3ab108791d72d32694169d0bcbe57bab2b641abd
Aaron Burk <aarondburk@gmail.com>
2018-06-12 19:12:31 -0500

    added boiler plate react rendering a list of state items

--- jobseekr/src/routes/jobs/index.jsx
+++ jobseekr/src/routes/jobs/index.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from '../../components/header';
 
-const Jobs = () =>
-  (
-    <div className="parent">
-      <Header />
-      <div className="JobsWrapper">
-        <div>
-          <h4>hello im Jobs
-          </h4>
+class Jobs extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      listTitles: [
+        'Wishlist',
+        'Applied',
+        'Phone',
+        'On Site',
+        'Offer',
+        'Rejected',
+        'Add List',
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="parent">
+        <Header />
+        <div className="jobs-list__header">
+          Jobs List
+        </div>
+        <div className="board">
+          {this.state.listTitles.map(title => (
+            <div className="list" key="">
+              {title}
+            </div>
+          ))}
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+}
 
 export default Jobs;
