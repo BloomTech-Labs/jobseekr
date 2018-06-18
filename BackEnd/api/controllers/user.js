1f45001722236a47ecd7b1a0ed811f1f736ece98
Aaron Burk <aarondburk@gmail.com>
2018-06-18 11:39:24 -0500

    removed nonworking commented code

--- BackEnd/api/controllers/user.js
+++ BackEnd/api/controllers/user.js
@@ -18,37 +18,6 @@ const createUser = (req, res) => {
   }
 };
 
-// const changePassword = (req, res) => {
-//   const { oldPassword, newPassword, id } = req.body;
-//   if (!oldPassword || !newPassword || !id) {
-//     res.status(400).send({ error: 'must provide both the old and new password and the user\'s id' });
-//   }
-//     // User.findById(id, (err, doc) => {
-//     //   if (err || doc === null) {
-//     //     res.status(400).send({ error: 'cannot find user by that id' })
-//     //   }
-//     //   doc.password = newPassword;
-//     //       doc.save(saveErr, savedUser)
-//     //       .then(savedUser => res.status(200).send(savedUser))
-//     //       .catch(saveErr => res.status(422).send(saveErr));
-//     //     });
-//     // console.log(Object.keys(changedUser));
-//     // console.log(changedUser.mongooseCollection);
-//     User.findOneAndUpdate(
-//       id,
-//       { password: newPassword },
-//       // runValidators: true, 
-//       (err, doc) => {
-//         doc.save()
-//         .then(doc => res.status(200).send(doc))
-//         .catch(err => res.status(422).send(err));
-//       },
-//     )    
-//   // } else {
-//   //   res.status(422).send('Please send your old password and a new password')
-//   // }
-//   }
-
 const changePassword = async (req, res) => {
   // - get token, old password, and new password from the request
   const { token, oldPassword, newPassword } = req.body;
