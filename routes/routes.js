070b49ae23e90a487134761b213f7c20b60ebb7e
Cliff Kang <inlovewithhim@gmail.com>
2018-06-19 13:33:12 -0700

    changed placement of server routes

--- routes/routes.js
+++ routes/routes.js
@@ -11,17 +11,17 @@ const path = require('path');
 module.exports = (server) => {
   // GET ROUTES
   server.route('/jobs').get(getAllJobs);
-  server.route('*').get((req, res) => {
-    res.sendFile(path.join(__dirname+'/FrontEnd/build/index.html'));
-  });
   
   // PUT ROUTES
   server.route('/changepassword').put(changePassword);
   server.route('/changeemail').put(changeEmail);
-
+  
   // DELETE ROUTES
-
+  
   // POST ROUTES
   server.route('/signup').post(createUser);
   server.route('/login').post(login);
+  server.route('*').get((req, res) => {
+    res.sendFile(path.join(__dirname+'/FrontEnd/build/index.html'));
+  });
 };
