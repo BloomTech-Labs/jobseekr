d91357bf5e332550de419750535e79d02e7f2635
Cliff Kang <inlovewithhim@gmail.com>
2018-06-19 13:51:28 -0700

    added /api to server routes

--- FrontEnd/src/routes/config.js
+++ FrontEnd/src/routes/config.js
@@ -1,3 +1,3 @@
-const ROOT_URL = 'https://ls-jobseekr1.herokuapp.com/';
+const ROOT_URL = 'https://ls-jobseekr1.herokuapp.com/api';
 
 export default ROOT_URL;
--- routes/routes.js
+++ routes/routes.js
@@ -10,17 +10,17 @@ const path = require('path');
 
 module.exports = (server) => {
   // GET ROUTES
-  server.route('/jobs').get(getAllJobs);
+  server.route('/api/jobs').get(getAllJobs);
   
   // PUT ROUTES
-  server.route('/changepassword').put(changePassword);
-  server.route('/changeemail').put(changeEmail);
+  server.route('/api/changepassword').put(changePassword);
+  server.route('/api/changeemail').put(changeEmail);
   
   // DELETE ROUTES
   
   // POST ROUTES
-  server.route('/signup').post(createUser);
-  server.route('/login').post(login);
+  server.route('/api/signup').post(createUser);
+  server.route('/api/login').post(login);
   server.route('*').get((req, res) => {
     res.sendFile(path.join(__dirname+'/FrontEnd/build/index.html'));
   });
