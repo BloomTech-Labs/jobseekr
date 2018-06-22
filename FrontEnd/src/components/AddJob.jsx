e27b0eba66d71549c9701fb5978456495f038046
Cliff Kang <inlovewithhim@gmail.com>
2018-06-22 12:59:51 -0700

    addJobModal v1, some basic functionality built-in
    -most text fields and selections are done
    -still needs to get hooked into the backend properly
    -will probably need some structural changes with backend integration
    -file upload still needs to be worked on

--- FrontEnd/src/components/AddJob.jsx
+++ FrontEnd/src/components/AddJob.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Popover, Button, Modal, OverlayTrigger, Radio, FormGroup, SplitButton, MenuItem, Glyphicon, Tooltip, Checkbox, FormControl } from 'react-bootstrap';
+import { ToggleButtonGroup, DropdownButton,ButtonToolbar, Button, Modal, OverlayTrigger, Radio, MenuItem, Glyphicon, Tooltip, Checkbox, FormControl } from 'react-bootstrap';
 
 class AddJob extends Component {
   constructor(props, context) {
@@ -9,25 +9,38 @@ class AddJob extends Component {
       show: false,
       timelineSelection: '',
       list: ['Want to Apply', 'Submitted Job App', 'Received Response', 'Phone Interview', 'On Site Interview', 'Technical Interview', 'Offer'],
+      gotRejected: false,
+      gotOffer: false,
+      notes: '',
+      companyName: '',
+      jobPostingLink: '',
+      pointOfContact : '',
+      sourceOfJob: ['Met in Person', 'Referral', 'Applied Online'],
+      sourceSelection: 'Source of Job',
     };
   }
 
-  handleTimelineRadioClick = (e) => {
-    console.log('event object is', e);
-    e.preventDefault();
-    const status = this.state;
-    console.log('event target prop is', e.target.prop);
-    console.log('event target attr is', e.target.attr);
-    status.timelineSelection = e.target.value;
-    this.setState(status);
+  handleTimelineRadioClick = (selection) => {
+    this.setState({ timelineSelection: selection });
+  }
+
+  handleCheckbox = (e) => {
+    const toBeChanged = e.target.value;
+    this.setState({ [toBeChanged] : !this.state[toBeChanged] })
+  }
+
+  handleChange = (e) => {
+    const change = e.target.value;
+    this.setState({ [e.target.id] : change });
+  }
+
+  handleSourceClick = (key, e) => {
+    const change = key;
+    console.log('change is', change);
+    this.setState({ sourceSelection: key });
   }
 
   render() {
-    // const popover = (
-    //   <Popover id="modal-popover" title="Add Job">
-    //     Add a new job to the list.
-    //   </Popover>
-    // );
     const tooltip = <Tooltip id="modal-tooltip">Add a Job.</Tooltip>;
 
     return (
@@ -47,42 +60,50 @@ class AddJob extends Component {
           <form>
             <Modal.Body>
               <div className="top-section-job-modal">
-                <FormGroup>
+                <ButtonToolbar>
                   <div className="top-left-section">
-                    {this.state.list.slice(0, Math.ceil(this.state.list.length / 2)).map(e => {
-                      return (
-                        <Radio 
-                          name='timeline' 
-                          value={e} 
-                          onClick={this.handleTimelineRadioClick}
-                          {...e === this.state.timelineSelection ? 'checked' : {}}>
-                          {e}
-                        </Radio>
-                      )
-                    })}
+                    <ToggleButtonGroup 
+                      type="radio" 
+                      name="timeline" 
+                      value={[this.state.timelineSelection]} 
+                      onChange={this.handleTimelineRadioClick}
+                    >
+                      {this.state.list.slice(0, Math.ceil(this.state.list.length / 2)).map(e => {
+                        return <Radio value={e}>{e}</Radio>
+                      })}
+                    </ToggleButtonGroup>
                   </div>
                   <div className="top-middle-section">
-                    {this.state.list.slice(Math.ceil(this.state.list.length / 2)).map(e => {
-                      return (
-                        <Radio 
-                          name='timeline' 
-                          value={e} 
-                          onClick={this.handleTimelineRadioClick}
-                          {...(e === this.state.timelineSelection ? 'checked' : {})}>
-                          {e}
-                        </Radio>
-                      )
-                    })}
+                    <ToggleButtonGroup 
+                      type="radio" 
+                      name="timeline" 
+                      value={[this.state.timelineSelection]} 
+                      onChange={this.handleTimelineRadioClick}
+                    >
+                      {this.state.list.slice(Math.ceil(this.state.list.length / 2)).map(e => {
+                        return <Radio value={e}>{e}</Radio>
+                      })}
+                    </ToggleButtonGroup>
                   </div>
-                </FormGroup>
+                </ButtonToolbar>
                 <div className="top-right-section">
-                  <Checkbox checked readOnly>
+                  <Checkbox 
+                    checked={this.state.gotRejected} 
+                    value={"gotRejected"}
+                    onChange={this.handleCheckbox} 
+                    readOnly
+                  >
                     Got a Rejection
                   </Checkbox>
                   <Button>
                     Upload Rejection Letter
                   </Button>
-                  <Checkbox unchecked readOnly>
+                  <Checkbox 
+                    checked={this.state.gotOffer} 
+                    value={"gotOffer"}
+                    onChange={this.handleCheckbox} 
+                    readOnly
+                  >
                     Got an Offer
                   </Checkbox>
                   <Button>
@@ -90,10 +111,13 @@ class AddJob extends Component {
                   </Button>
                 </div>
               </div>
-              <FormControl componentClass="textarea" placeholder="Notes" />
-              {/* <OverlayTrigger overlay={popover}>
-                <a href="#popover">popover</a>
-              </OverlayTrigger>{' '} */}
+              <FormControl 
+                componentClass="textarea" 
+                value={this.state.notes} 
+                placeholder="Notes"
+                id='notes'
+                onChange={this.handleChange}
+              />
             </Modal.Body>
             <Modal.Header closeButton>
               <Modal.Title>Job Information</Modal.Title>
@@ -101,16 +125,31 @@ class AddJob extends Component {
             <Modal.Body>
               <div className="bottom-section-job-modal">
                 <div className="bottom-left-section">
-                  <FormControl type="text" placeholder="Company Name" />
-                  <FormControl type="url" placeholder="Link to Job Posting" />
-                  <FormControl type="text" placeholder="Point of Contact" />
+                  <FormControl 
+                    type="text" 
+                    placeholder="Company Name" 
+                    id='companyName'
+                    onChange={this.handleChange}
+                  />
+                  <FormControl 
+                    type="text" 
+                    placeholder="Link to Job Posting" 
+                    id='jobPostingLink'
+                    onChange={this.handleChange}
+                  />
+                  <FormControl 
+                    type="text" 
+                    placeholder="Point of Contact" 
+                    id='pointOfContact'
+                    onChange={this.handleChange}
+                  />
                 </div>
                 <div className="bottom-right-section">
-                  <SplitButton title="Source of Job">
-                    <MenuItem eventKey="1">Met in Person</MenuItem>
-                    <MenuItem eventKey="2">Applied Online</MenuItem>
-                    <MenuItem eventKey="3">Referral</MenuItem>
-                  </SplitButton>
+                  <DropdownButton title={this.state.sourceSelection}>
+                    {this.state.sourceOfJob.map(e => {
+                      return <MenuItem eventKey={e} onSelect={this.handleSourceClick}>{e}</MenuItem>
+                    })}
+                  </DropdownButton>
                   <Button>
                     Resolution (Open/Closed)
                   </Button>
