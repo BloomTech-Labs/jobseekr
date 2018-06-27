5c3f5eaaa1a1c78955d989598abb6e51c6533d3b
Aaron Burk <aarondburk@gmail.com>
2018-06-27 17:44:39 -0500

    fix conflicts

--- controllers/index.js
+++ controllers/index.js
@@ -1,17 +1,12 @@
 const { createUser, changePassword, changeEmail } = require('./user');
 const login = require('./login');
-<<<<<<< HEAD
 const { getAllJobs, createJob, editJob } = require('./jobs');
-const { createMeetup } = require('./meetups');
-=======
-const { getAllJobs } = require('./jobs');
 const { createMeetup, getAllMeetups, destroyMeetup } = require('./meetups');
 const {
   getAllContributions,
   createContribution,
   destroyContribution, } = require('./contributions')
 const { billing } = require("./billing");
->>>>>>> d8a824a21023a9265a91d786606b3aead1db460e
 
 module.exports = {
   createUser,
@@ -20,15 +15,12 @@ module.exports = {
   changePassword,
   changeEmail,
   createMeetup,
-<<<<<<< HEAD
   createJob,
   editJob,
-=======
   billing,
   getAllMeetups,
   destroyMeetup,
   getAllContributions,
   createContribution,
   destroyContribution,
->>>>>>> d8a824a21023a9265a91d786606b3aead1db460e
 };
--- routes/routes.js
+++ routes/routes.js
@@ -6,13 +6,8 @@ const {
   changeEmail,
   billing,
   createMeetup,
-<<<<<<< HEAD
   createJob,
   editJob,
-} = require('../controllers');
-
-module.exports = (server) => {
-=======
   getAllMeetups,
   destroyMeetup,
   getAllContributions,
@@ -20,24 +15,17 @@ module.exports = (server) => {
   destroyContribution,
 } = require('../controllers');
 
-module.exports = server => {
->>>>>>> d8a824a21023a9265a91d786606b3aead1db460e
+module.exports = (server) => {
   // GET ROUTES
   server.route('/api/jobs').get(getAllJobs);
   server.route('/api/meetups').get(getAllMeetups);
   server.route('/api/contributions').get(getAllContributions);
   
   // PUT ROUTES
-<<<<<<< HEAD
   server.route('/api/changepassword').put(changePassword);
   server.route('/api/changeemail').put(changeEmail);
   server.route('/api/jobs').put(editJob);
   
-=======
-  server.route("/api/changepassword").put(changePassword);
-  server.route("/api/changeemail").put(changeEmail);
-
->>>>>>> d8a824a21023a9265a91d786606b3aead1db460e
   // DELETE ROUTES
   server.route('/api/meetups').delete(destroyMeetup);
   server.route('/api/contributions').delete(destroyContribution);
@@ -46,11 +34,8 @@ module.exports = server => {
   server.route('/api/signup').post(createUser);
   server.route('/api/login').post(login);
   server.route('/api/meetups').post(createMeetup);
-<<<<<<< HEAD
   server.route('/api/jobs').post(createJob);
-=======
   server.route('/api/contributions').post(createContribution);
   server.route("/api/billing").post(billing);
  
->>>>>>> d8a824a21023a9265a91d786606b3aead1db460e
 };
