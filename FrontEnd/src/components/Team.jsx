c20b126bd0ede648151cedad835d9783d53c418e
Aaron Burk <aarondburk@gmail.com>
2018-06-29 11:49:25 -0500

    profile pictures reformat

--- FrontEnd/src/components/Team.jsx
+++ FrontEnd/src/components/Team.jsx
@@ -1,123 +1,46 @@
 import React from 'react';
-import { Media, Image } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
 import Team1 from '../Images/Daniel.png';
 import Team2 from '../Images/Jon.png';
-import Team3 from '../Images/Aaron.png';
+import Team3 from '../Images/AaronProfile.png';
 import Team4 from '../Images/Cliff.png';
 
 
 const Team = () =>
   (
     <div className="TeamWrapper">
-      <Media>
-        <h3 className="teamh3">
-          <strong>
+      <h3 className="teamh3">
+        <strong>
           The Dream Team:
-          </strong>
-        </h3>
-        <Media.Left align="top">
-          <Image width={164} height={164} src={Team1} alt="thumbnail" circle />
-        </Media.Left>
-        <Media.Body>
-          <Media.Heading>Daniel Lara - BackEnd Ninja</Media.Heading>
-          <p>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-            ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-            tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-            fringilla. Donec lacinia congue felis in faucibus.
-          </p>
-
-          <p>
-            Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu
-            leo. Cum sociis natoque penatibus et magnis dis parturient montes,
-            nascetur ridiculus mus.
-          </p>
-        </Media.Body>
-      </Media>
-      <Media>
-        <Media.Left align="top">
-          <Image width={164} height={164} src={Team2} alt="thumbnail" circle />
-        </Media.Left>
-        <Media.Body>
-          <Media.Heading>Jonathan Bry - FrontEnd Wizard</Media.Heading>
-          <p>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-            ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-            tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-            fringilla. Donec lacinia congue felis in faucibus.
-          </p>
-
-          <p>
-            Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu
-            leo. Cum sociis natoque penatibus et magnis dis parturient montes,
-            nascetur ridiculus mus.
-          </p>
-        </Media.Body>
-      </Media>
-      <Media>
-        <Media.Left align="top">
-          <Image width={164} height={164} src={Team3} alt="thumbnail" circle />
-        </Media.Left>
-        <Media.Body>
-          <Media.Heading>Aaron Berk - Epitome Of Fullstack </Media.Heading>
-          <p>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-            ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-            tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-            fringilla. Donec lacinia congue felis in faucibus.
-          </p>
-
-          <p>
-            Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu
-            leo. Cum sociis natoque penatibus et magnis dis parturient montes,
-            nascetur ridiculus mus.
-          </p>
-        </Media.Body>
-      </Media>
-      <Media>
-        <Media.Left align="top">
-          <Image width={164} height={164} src={Team4} alt="thumbnail" circle />
-        </Media.Left>
-        <Media.Body>
-          <Media.Heading>Cliff Kang - Full Stack Superstar</Media.Heading>
-          <p>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-            ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-            tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-            fringilla. Donec lacinia congue felis in faucibus.
-          </p>
-
-          <p>
-            Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu
-            leo. Cum sociis natoque penatibus et magnis dis parturient montes,
-            nascetur ridiculus mus.
-          </p>
-        </Media.Body>
-      </Media>
+        </strong>
+      </h3>
+      <div className="profiles__container">
+      <div className="profiles__rt-container">
+      <div className="lp-profile-pic">
+        <Image width={164} height={164} src={Team1} alt="thumbnail" circle />
+        <h3>Daniel Lara</h3>
+        <p>BackEnd Ninja</p>
+      </div>
+      <div className="lp-profile-pic">
+        <Image width={164} height={164} src={Team2} alt="thumbnail" circle />
+        <h3>Jonathan Bry</h3>
+        <p>FrontEnd Wizard</p>
+      </div>
+      </div>
+      <div className="profiles__lt-container">
+      <div className="lp-profile-pic">
+        <Image width={164} height={164} src={Team3} alt="thumbnail" circle />
+        <h3>Aaron Burk</h3>
+        <p>Epitome Of Fullstack</p>
+      </div>
+      <div className="lp-profile-pic">
+        <Image width={164} height={164} src={Team4} alt="thumbnail" circle />
+        <h3>Cliff Kang</h3>
+        <p>Full Stack Superstar</p>
+      </div>
+      </div>
+      </div>
     </div>
   );
 // add href to people's websites
 export default Team;
-
-// import React from 'react';
-
-// const Team = () =>
-//   (
-//     <div className="TeamWrapper">
-//       <h3>
-//         Our Team
-//       </h3>
-//       <div className="card">
-//         <img className="card-img-top" src="..." alt="Car" />
-//         <div className="card-body">
-//           <h5 className="card-title">Card title</h5>
-//           <p className="card-text">
-//            Some quick example text to build on the card title
-//            and make up the bulk of the cards content.</p>
-//           <a href="/" className="btn btn-primary">Go somewhere</a>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// // add href to people's websites
-// export default Team;
