bb293d6bebe0ecb1864743cdc062dd55ae88075e
endtrippy@gmail.com <endtrippy@gmail.com>
2018-07-09 09:43:00 -0700

    Fixed SignUp - App now works right after signing up. No need to re-login

--- controllers/user.js
+++ controllers/user.js
@@ -4,12 +4,27 @@ const jwt = require('jsonwebtoken');
 const mySecret = process.env.SECRET || 'random';
 
 const createUser = (req, res) => {
-  const { email, password } = req.body;
+  const {
+    email,
+    password
+  } = req.body;
   if (email && password) {
-    const newUser = new User({ email, password });
+    const newUser = new User({
+      email,
+      password
+    });
     newUser
       .save()
-      .then(user => res.status(200).send(user))
+      .then(user => {
+        const payload = {
+          email: user.email
+        };
+        const token = jwt.sign(payload, mySecret.toString());
+        res.json({
+          token
+        });
+
+      })
       .catch(err => {
         console.log(err);
         res.status(422).send(err);
@@ -21,18 +36,28 @@ const createUser = (req, res) => {
 
 const changePassword = async (req, res) => {
   // - get token, old password, and new password from the request
-  const { token, oldPassword, newPassword } = req.body;
+  const {
+    token,
+    oldPassword,
+    newPassword
+  } = req.body;
   //- use jwt.verify to find email
   const storedPayload = await jwt.verify(token, mySecret);
   const email = storedPayload.email;
   //- find user using email
-  User.findOne({ email }, (err, user) => {
+  User.findOne({
+    email
+  }, (err, user) => {
     if (err) {
-      res.status(403).json({ error: 'Invalid Email/Password' });
+      res.status(403).json({
+        error: 'Invalid Email/Password'
+      });
       return;
     }
     if (user === null) {
-      res.status(422).json({ error: 'No user with that email in our records' });
+      res.status(422).json({
+        error: 'No user with that email in our records'
+      });
       return;
     }
     //- - make sure that the current password matches old password (got code from login controller)
@@ -40,16 +65,27 @@ const changePassword = async (req, res) => {
       if (nonMatch !== null) {
         res
           .status(422)
-          .json({ error: 'old password does not match our stored password' });
+          .json({
+            error: 'old password does not match our stored password'
+          });
         return;
       }
       //- - if matches, change password to the new password
       if (hashMatch) {
         bcrypt.hash(newPassword, 11, (err, hash) => {
-          User.findOneAndUpdate({ email }, { password: hash }, { new: true })
+          User.findOneAndUpdate({
+              email
+            }, {
+              password: hash
+            }, {
+              new: true
+            })
             .then(user => res.status(200).send(user))
             .catch(err => {
-              res.status(422).json({ error: 'error updating password', err });
+              res.status(422).json({
+                error: 'error updating password',
+                err
+              });
             });
         });
       }
@@ -59,22 +95,32 @@ const changePassword = async (req, res) => {
 
 const changeEmail = async (req, res) => {
   // - get token, old email, and new email from the request
-  const { token, newEmail } = req.body;
-  let { oldEmail } = req.body;
+  const {
+    token,
+    newEmail
+  } = req.body;
+  let {
+    oldEmail
+  } = req.body;
   //- use jwt.verify to find email
   const storedPayload = await jwt.verify(token, mySecret);
   const storedEmail = storedPayload.email;
   oldEmail = oldEmail.toLowerCase();
   if (storedEmail === oldEmail) {
     //- find user using email
-    User.findOneAndUpdate(
-      { email: oldEmail },
-      { email: newEmail },
-      { new: true }
-    )
+    User.findOneAndUpdate({
+        email: oldEmail
+      }, {
+        email: newEmail
+      }, {
+        new: true
+      })
       .then(user => res.status(200).send(user))
       .catch(err => {
-        res.status(422).json({ error: 'error updating email', err });
+        res.status(422).json({
+          error: 'error updating email',
+          err
+        });
       });
   } else {
     res.status(422).json({
